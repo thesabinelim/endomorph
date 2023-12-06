02c0f27dbc4847a7f2ca8206b5c029865217cb77
Sabine Lim <sabine@canva.com>
2023-12-06 13:49:43 +0900

    Input loop

--- rem/src/main.rs
+++ rem/src/main.rs
@@ -1,5 +1,27 @@
+use std::io::{self, Write};
+
+const NAME: &str = env!("CARGO_PKG_NAME");
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 fn main() {
-    println!("Endomorph {}", VERSION);
+    println!("{} {}", NAME, VERSION);
+    while let Ok(Some(line)) = prompt() {
+        println!("{}", line);
+    }
+}
+
+fn prompt() -> Result<Option<String>, io::Error> {
+    print!("> ");
+    io::stdout().flush()?;
+    read_line()
+}
+
+fn read_line() -> Result<Option<String>, io::Error> {
+    let mut line = String::new();
+    let len = io::stdin().read_line(&mut line)?;
+    if len > 0 {
+        Ok(Some(line))
+    } else {
+        Ok(None)
+    }
 }
