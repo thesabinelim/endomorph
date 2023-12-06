6f4c17729c52c395e4d3a2fafe5f0c92f116b860
Sabine Lim <sabine@canva.com>
2023-12-06 15:27:06 +0900

    Token

---
+++ rem/src/data/token.rs
@@ -0,0 +1,90 @@
+pub enum Token {
+    Comment(Comment),
+    EndOfInput,
+    Identifier(Identifier),
+    Keyword(Keyword),
+    Literal(Literal),
+    Operator(Operator),
+    Punctuator(Punctuator),
+    Whitespace(Whitespace),
+}
+
+pub struct Comment {
+    text: String,
+}
+
+pub struct Identifier {
+    name: String,
+}
+
+pub enum Keyword {
+    Else,
+    Export,
+    For,
+    From,
+    If,
+    In,
+    Import,
+    Let,
+    Mut,
+}
+
+pub enum Literal {
+    Boolean,
+    Char,
+    Integer,
+    Float,
+    String,
+}
+
+pub enum Operator {
+    And,
+    AndAssign,
+    Arrow,
+    Assign,
+    Divide,
+    DivideAssign,
+    Dot,
+    Equality,
+    Greater,
+    GreaterEquals,
+    Less,
+    LessEquals,
+    LShift,
+    LShiftAssign,
+    Minus,
+    MinusAssign,
+    Modulo,
+    ModuloAssign,
+    Not,
+    NotEquals,
+    Or,
+    OrAssign,
+    Plus,
+    PlusAssign,
+    Power,
+    PowerAssign,
+    Ternary,
+    RShift,
+    RShiftAssign,
+    Spread,
+    Times,
+    TimesAssign,
+    Xor,
+    XorAssign,
+}
+
+pub enum Punctuator {
+    Colon,
+    Comma,
+    LBrace,
+    LBracket,
+    LParens,
+    RBrace,
+    RBracket,
+    RParens,
+}
+
+pub struct Whitespace {
+    text: String,
+}
