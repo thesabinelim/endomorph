33788028f26f37e3aa9a74c13266ccb5d5977fcb
Sabine Lim <sabine@canva.com>
2023-12-08 22:24:11 +0900

    Add Box

--- rem/src/parser_combinators/mod.rs
+++ rem/src/parser_combinators/mod.rs
@@ -5,4 +5,5 @@ pub trait TokenStream<Token> {
 
 // For performance reasons, a parser directly modifies the token stream via the
 // trait methods on consumption rather than return a copy for the remainder
-pub type Parser<Token, Production> = fn(dyn TokenStream<Token>) -> Result<Production, String>;
+pub type Parser<Token, Production> =
+    dyn Fn(Box<dyn TokenStream<Token>>) -> Result<Production, String>;
