867ee6bc847f14808601331906082035a05509c9
Sabine Lim <sabine@canva.com>
2023-12-09 14:37:30 +0900

    Change test calls

--- rem/src/parser_combinators/tests/parser.rs
+++ rem/src/parser_combinators/tests/parser.rs
@@ -7,30 +7,35 @@ use super::TestToken;
 
 #[test]
 fn describe_eof_it_works() {
-    let result = eof()(test_tokens![]);
-    assert!(result.is_ok())
+    let parser = eof();
+    let stream = test_tokens![];
+    assert!(parser(stream).is_ok())
 }
 
 #[test]
 fn describe_eof_it_errors_on_not_eof() {
-    let result = eof()(test_tokens![TestToken::A]);
-    assert!(result.is_err())
+    let parser = eof();
+    let stream = test_tokens![TestToken::A];
+    assert!(parser(stream).is_err())
 }
 
 #[test]
 fn describe_single_it_works() {
-    let result = single(TestToken::A)(test_tokens![TestToken::A]);
-    assert!(result.is_ok_and(|production| production == TestToken::A));
+    let parser = single(TestToken::A);
+    let stream = test_tokens![TestToken::A];
+    assert!(parser(stream).is_ok_and(|production| production == TestToken::A));
 }
 
 #[test]
 fn describe_single_it_errors_on_mismatch() {
-    let result = single(TestToken::A)(test_tokens![TestToken::B]);
-    assert!(result.is_err());
+    let parser = single(TestToken::A);
+    let stream = test_tokens![TestToken::B];
+    assert!(parser(stream).is_err());
 }
 
 #[test]
 fn describe_single_it_errors_on_eof() {
-    let result = single(TestToken::A)(test_tokens![]);
-    assert!(result.is_err());
+    let parser = single(TestToken::A);
+    let stream = test_tokens![];
+    assert!(parser(stream).is_err());
 }
