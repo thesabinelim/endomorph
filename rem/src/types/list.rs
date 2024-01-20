88390c17a6043c15c09bac8980eb0454e4d0fc72
Sabine Lim <i@sab.gg>
2024-01-20 23:41:13 +1100

    append and prepend

--- rem/src/types/list.rs
+++ rem/src/types/list.rs
@@ -4,52 +4,43 @@ macro_rules! list {
     };
     ($item:expr) => { $crate::types::list::list![$item,] };
     ($item:expr, $($rest:tt)*) => {
-        $crate::types::list::Cons::new($item, $crate::types::list::list![$($rest)*])
+        $crate::types::list::Cons($item, $crate::types::list::list![$($rest)*])
     };
 }
 
 pub(crate) use list;
 
-#[derive(Clone, Debug)]
-pub struct Cons<Item, Rest>
-where
-    Rest: List,
-{
-    item: Item,
-    rest: Rest,
-}
-
-impl<Item, Rest> Cons<Item, Rest>
-where
-    Rest: List,
-{
-    pub fn new(item: Item, rest: Rest) -> Cons<Item, Rest> {
-        Cons { item, rest }
-    }
-}
-
-#[derive(Clone, Debug)]
-pub struct Nil;
-
-pub trait List
-where
-    Self: Clone + PartialEq,
-{
-    const LEN: usize;
-}
-
-impl<Item, Rest> Cons<Item, Rest>
+#[derive(Clone, PartialEq, Debug)]
+pub struct Cons<Item, Rest>(pub Item, pub Rest)
 where
     Item: Clone + PartialEq,
-    Rest: List,
-{
-    fn head(&self) -> Item {
-        self.item.clone()
+    Rest: List;
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct Nil;
+
+pub trait List: Clone + PartialEq {
+    const LEN: usize;
+
+    fn len(&self) -> usize {
+        Self::LEN
     }
 
-    fn rest(&self) -> Rest {
-        self.rest.clone()
-    }
+    type AppendResult<T>: List
+    where
+        T: Clone + PartialEq;
+
+    fn append<T>(&self, item: T) -> Self::AppendResult<T>
+    where
+        T: Clone + PartialEq;
+
+    type PrependResult<T>: List
+    where
+        T: Clone + PartialEq;
+
+    fn prepend<T>(&self, item: T) -> Self::PrependResult<T>
+    where
+        T: Clone + PartialEq;
 }
 
 impl<Item, Rest> List for Cons<Item, Rest>
@@ -58,16 +49,28 @@ where
     Rest: List,
 {
     const LEN: usize = Rest::LEN + 1;
-}
 
-impl<Item, Rest, Rhs> PartialEq<Rhs> for Cons<Item, Rest>
-where
-    Self: List,
-    Rest: List,
-    Rhs: List,
-{
-    fn eq(&self, _other: &Rhs) -> bool {
-        Self::LEN == Rhs::LEN
+    type AppendResult<T> = Cons<Item, Rest::AppendResult<T>>
+    where
+        T: Clone + PartialEq;
+
+    fn append<T>(&self, item: T) -> Self::AppendResult<T>
+    where
+        T: Clone + PartialEq,
+    {
+        let Cons(self_item, rest) = self;
+        Cons(self_item.clone(), rest.append(item))
+    }
+
+    type PrependResult<T> = Cons<T, Self>
+    where
+        T: Clone + PartialEq;
+
+    fn prepend<T>(&self, item: T) -> Self::PrependResult<T>
+    where
+        T: Clone + PartialEq,
+    {
+        Cons(item, self.clone())
     }
 }
 
@@ -75,13 +78,26 @@ impl Nil {}
 
 impl List for Nil {
     const LEN: usize = 0;
-}
 
-impl<Rhs> PartialEq<Rhs> for Nil
-where
-    Rhs: List,
-{
-    fn eq(&self, _other: &Rhs) -> bool {
-        Self::LEN == Rhs::LEN
+    type AppendResult<T> = Cons<T, Self>
+    where
+        T: Clone + PartialEq;
+
+    fn append<T>(&self, item: T) -> Self::AppendResult<T>
+    where
+        T: Clone + PartialEq,
+    {
+        Cons(item, Nil)
+    }
+
+    type PrependResult<T> = Cons<T, Self>
+    where
+        T: Clone + PartialEq;
+
+    fn prepend<T>(&self, item: T) -> Self::PrependResult<T>
+    where
+        T: Clone + PartialEq,
+    {
+        Cons(item, Nil)
     }
 }
--- rem/src/types/tests/list.rs
+++ rem/src/types/tests/list.rs
@@ -1,4 +1,14 @@
-use crate::types::list::list;
+use crate::types::list::{list, Cons, List};
+
+#[test]
+fn describe_list_len_it_works() {
+    assert_eq!(list![1, true, "three"].len(), 3)
+}
+
+#[test]
+fn describe_list_len_it_returns_zero_when_empty() {
+    assert_eq!(list![].len(), 0)
+}
 
 #[test]
 fn describe_list_eq_it_returns_true_when_both_empty() {
@@ -6,16 +16,44 @@ fn describe_list_eq_it_returns_true_when_both_empty() {
 }
 
 #[test]
-fn describe_list_eq_it_returns_false_with_different_lengths() {
-    assert_ne!(list![1, 2, 3], list![1, 2, 3, 4])
-}
-
-#[test]
 fn describe_list_eq_it_returns_true_with_matching_elements() {
-    assert_eq!(list![1, 2, 3], list![1, 2, 3])
+    assert_eq!(list![1, true, "three"], list![1, true, "three"])
 }
 
 #[test]
 fn describe_list_eq_it_returns_false_with_mismatching_elements() {
-    assert_ne!(list![1, 2, 3], list![3, 2, 1])
+    assert_ne!(list![1, true, "three"], list![1, true, "mismatch"])
+}
+
+#[test]
+fn describe_list_pattern_matching_it_works() {
+    let Cons(item, rest) = list![1, true, "three"];
+    assert_eq!(item, 1);
+    assert_eq!(rest, list![true, "three"])
+}
+
+#[test]
+fn describe_list_append_it_works() {
+    assert_eq!(
+        list![1, true, "three"].append('4'),
+        list![1, true, "three", '4']
+    )
+}
+
+#[test]
+fn describe_list_append_it_works_when_empty() {
+    assert_eq!(list![].append(1), list![1])
+}
+
+#[test]
+fn describe_list_prepend_it_works() {
+    assert_eq!(
+        list![1, true, "three"].prepend('4'),
+        list!['4', 1, true, "three"]
+    )
+}
+
+#[test]
+fn describe_list_prepend_it_works_when_empty() {
+    assert_eq!(list![].append(1), list![1])
 }
