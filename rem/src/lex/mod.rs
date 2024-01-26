5cd6af74ae291af87fcee4fa0df3e95a61fbb41a
Sabine Lim <i@sab.gg>
2024-01-26 18:29:56 +1100

    Turn these off for now

--- rem/src/lex/mod.rs
+++ rem/src/lex/mod.rs
@@ -1,31 +1,31 @@
-use crate::types::{
-    source::{Source, SourceError},
-    token::Token,
-};
+// use crate::types::{
+//     source::{Source, SourceError},
+//     token::Token,
+// };
 
-pub struct PartialLexInput {
-    pub offset: usize,
-    pub remaining: String,
-}
+// pub struct PartialLexInput {
+//     pub offset: usize,
+//     pub remaining: String,
+// }
 
-pub type PartialLexResult = Result<PartialLexSuccess, SourceError>;
+// pub type PartialLexResult = Result<PartialLexSuccess, SourceError>;
 
-pub struct PartialLexSuccess {
-    pub token: Token,
-    pub next_input: PartialLexInput,
-}
+// pub struct PartialLexSuccess {
+//     pub token: Token,
+//     pub next_input: PartialLexInput,
+// }
 
-pub fn lex(source: &Source) -> Result<Vec<Token>, SourceError> {
-    let success = token(PartialLexInput {
-        offset: 0,
-        remaining: source.text.clone(),
-    })?;
-    Ok(vec![success.token])
-}
+// pub fn lex(source: &Source) -> Result<Vec<Token>, SourceError> {
+//     let success = token(PartialLexInput {
+//         offset: 0,
+//         remaining: source.text.clone(),
+//     })?;
+//     Ok(vec![success.token])
+// }
 
-fn token(input: PartialLexInput) -> PartialLexResult {
-    Err(SourceError {
-        offset: input.offset,
-        message: "Lexer unimplemented".to_string(),
-    })
-}
+// fn token(input: PartialLexInput) -> PartialLexResult {
+//     Err(SourceError {
+//         offset: input.offset,
+//         message: "Lexer unimplemented".to_string(),
+//     })
+// }
--- rem/src/main.rs
+++ rem/src/main.rs
@@ -4,63 +4,63 @@ mod parser_combinators;
 mod types;
 mod util;
 
-use io::prompt_line;
-use lex::lex;
-use types::{
-    source::{Source, SourceError},
-    token::{Token, TokenKind},
-};
-use util::source::offset_to_position;
-use util::text::remove_line_break;
+// use io::prompt_line;
+// use lex::lex;
+// use types::{
+//     source::{Source, SourceError},
+//     token::{Token, TokenKind},
+// };
+// use util::source::offset_to_position;
+// use util::text::remove_line_break;
 
 const NAME: &str = env!("CARGO_PKG_NAME");
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 fn main() {
     println!("{} {}", NAME, VERSION);
-    while let Ok(Some(line)) = prompt_line() {
-        process_text(remove_line_break(line));
-    }
+    // while let Ok(Some(line)) = prompt_line() {
+    //     process_text(remove_line_break(line));
+    // }
 }
 
-fn process_text(text: String) {
-    let source = Source {
-        file: "stdin".to_string(),
-        text,
-    };
-    match lex(&source) {
-        Ok(tokens) => print_tokens(tokens),
-        Err(error) => print_error(&source, error),
-    }
-}
+// fn process_text(text: String) {
+//     let source = Source {
+//         file: "stdin".to_string(),
+//         text,
+//     };
+//     match lex(&source) {
+//         Ok(tokens) => print_tokens(tokens),
+//         Err(error) => print_error(&source, error),
+//     }
+// }
 
-fn print_tokens(tokens: Vec<Token>) {
-    println!(
-        "[{}]",
-        tokens
-            .iter()
-            .map(|token| match token.kind {
-                TokenKind::Comment(_) => "Comment",
-                TokenKind::EndOfInput => "EndOfInput",
-                TokenKind::Identifier(_) => "Identifier",
-                TokenKind::Keyword(_) => "Keyword",
-                TokenKind::Literal(_) => "Literal",
-                TokenKind::Operator(_) => "Operator",
-                TokenKind::Punctuator(_) => "Punctuator",
-                TokenKind::Whitespace(_) => "Whitespace",
-            })
-            .collect::<Vec<&str>>()
-            .join(", ")
-    );
-}
+// fn print_tokens(tokens: Vec<Token>) {
+//     println!(
+//         "[{}]",
+//         tokens
+//             .iter()
+//             .map(|token| match token.kind {
+//                 TokenKind::Comment(_) => "Comment",
+//                 TokenKind::EndOfInput => "EndOfInput",
+//                 TokenKind::Identifier(_) => "Identifier",
+//                 TokenKind::Keyword(_) => "Keyword",
+//                 TokenKind::Literal(_) => "Literal",
+//                 TokenKind::Operator(_) => "Operator",
+//                 TokenKind::Punctuator(_) => "Punctuator",
+//                 TokenKind::Whitespace(_) => "Whitespace",
+//             })
+//             .collect::<Vec<&str>>()
+//             .join(", ")
+//     );
+// }
 
-fn print_error(source: &Source, error: SourceError) {
-    let position = offset_to_position(&source, error.offset);
-    println!(
-        "{}:{}:{} {}",
-        source.file,
-        position.line + 1,
-        position.col + 1,
-        error.message
-    );
-}
+// fn print_error(source: &Source, error: SourceError) {
+//     let position = offset_to_position(&source, error.offset);
+//     println!(
+//         "{}:{}:{} {}",
+//         source.file,
+//         position.line + 1,
+//         position.col + 1,
+//         error.message
+//     );
+// }
