b710749d9fe2e9895721e47c0b807ef6e64df473
Sabine Lim <i@sab.gg>
2024-01-28 16:10:44 +1100

    Move import

--- rem/src/main.rs
+++ rem/src/main.rs
@@ -1,11 +1,11 @@
+use cli::repl::repl;
+
 mod cli;
 mod diagnostics;
 mod lex;
 mod parser_combinators;
 mod types;
 mod util;
-
-use cli::repl::repl;
 
 const NAME: &str = env!("CARGO_PKG_NAME");
 const VERSION: &str = env!("CARGO_PKG_VERSION");
